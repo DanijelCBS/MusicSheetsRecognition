b68063d33aed20f909972897d9c4e1d1af4a96fc
Danijel Radulović <master.daca09@gmail.com>
2020-02-18 13:31:55 +0100

    Add weights after 16 epochs of training

--- model/training.py
+++ model/training.py
@@ -6,12 +6,14 @@ from sequence_train import SequenceFactory
 
 def train_network(model, train_generator, val_generator):
     weights_path = "weights-{epoch:02d}-{loss:.4f}.hdf5"
-    checkpoint = ModelCheckpoint(weights_path, monitor='loss', verbose=0, save_best_only=True, mode='min')
+    checkpoint = ModelCheckpoint(weights_path, monitor='loss', verbose=0, save_best_only=True, mode='min',
+                                 save_weights_only=True)
     batch_log = LambdaCallback(on_batch_end=batch_output)
     callbacks_list = [checkpoint, batch_log]
 
     model.fit_generator(train_generator, epochs=16, callbacks=callbacks_list, validation_data=val_generator,
-                        validation_freq=4, workers=2, use_multiprocessing=True)
+                        validation_freq=4, workers=1, use_multiprocessing=False, steps_per_epoch=len(train_generator),
+                        validation_steps=len(val_generator))
 
 
 def batch_output(batch, logs):
@@ -20,7 +22,7 @@ def batch_output(batch, logs):
 
 
 if __name__ == '__main__':
-    image_height = 64
+    image_height = 32
     batch_size = 8
     sequence_factory = SequenceFactory('data/primus_dataset', 'data/train.txt',
                                        'data/vocabulary_semantic.txt',
